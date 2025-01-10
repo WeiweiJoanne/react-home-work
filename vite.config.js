414d6927d693200cb1bdb1685562a06443fb6499
Joanne Wei <joannewei0921@gmail.com>
2025-01-11 04:43:46 +0800

    update base settings

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-home-work/', // 填入你的 GitHub repository 名稱
+  build: {
+    outDir: 'dist', // 確保輸出資料夾是 dist
+  },
 })
